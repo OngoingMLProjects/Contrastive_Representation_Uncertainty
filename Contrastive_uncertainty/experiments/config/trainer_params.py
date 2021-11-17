5b9e8df5d7ba888730e74ba2ba4fde70decc0b14
nerdk312 <nawid.keshtmand@hotmail.com>
2021-11-17 16:22:48 +0000

    commiting before changing the code to enable calculating the AUROC using the normal mahalanobis distance and the softmax probability

--- Contrastive_uncertainty/experiments/config/trainer_params.py
+++ Contrastive_uncertainty/experiments/config/trainer_params.py
@@ -5,9 +5,9 @@ trainer_hparams = dict(
 seed = 26,
 epochs = 0,
 bsz =8, #128, #32,#64,
-#OOD_dataset = ['STL10', 'CelebA','WIDERFace','SVHN', 'Caltech101','Caltech256','CIFAR10','CIFAR100', 'VOC', 'Places365','TinyImageNet','Cub200','Dogs', 'MNIST', 'FashionMNIST', 'KMNIST', 'EMNIST'],
+OOD_dataset = ['STL10', 'CelebA','WIDERFace','SVHN', 'Caltech101','Caltech256','CIFAR10','CIFAR100', 'VOC', 'Places365','TinyImageNet','Cub200','Dogs', 'MNIST', 'FashionMNIST', 'KMNIST', 'EMNIST'],
 #OOD_dataset = ['MNIST', 'FashionMNIST', 'KMNIST', 'EMNIST'],
-OOD_dataset = ['KMNIST'],
+#OOD_dataset = ['KMNIST'],
 #OOD_dataset =['MNIST','FashionMNIST','KMNIST','EMNIST','Places365','VOC'],
 # Trainer configurations
 fast_run = False,
