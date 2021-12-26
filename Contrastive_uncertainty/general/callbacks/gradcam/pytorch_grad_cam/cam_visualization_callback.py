2a5a070d1ad4bb02cd7830e5d6ef85af9e306f18
nerdk312 <nawid.keshtmand@hotmail.com>
2021-12-26 11:34:39 +0000

    able to run the code for being able to use gradcam as well as visualize, additional changes required to make it usable for moco

--- Contrastive_uncertainty/general/callbacks/gradcam/pytorch_grad_cam/cam_visualization_callback.py
+++ Contrastive_uncertainty/general/callbacks/gradcam/pytorch_grad_cam/cam_visualization_callback.py
@@ -1,29 +1,22 @@
-from numpy.core.numeric import indices
-from scipy.special.orthogonal import orthopoly1d
 import torch
 from torch.autograd import grad
-import torch.optim as optim
-import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Dirichlet
+import torchvision
 import pytorch_lightning as pl
 
 import os
 import numpy as np
-import sklearn.datasets
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import wandb
 import sklearn.metrics as skm
-import faiss
-import statistics 
-import scipy
 from PIL import Image
 
 from Contrastive_uncertainty.general.callbacks.general_callbacks import quickloading
 from Contrastive_uncertainty.general.callbacks.gradcam.pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM
 from Contrastive_uncertainty.general.callbacks.gradcam.pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+
 # Based on code from https://github.com/jacobgil/pytorch-grad-cam
 class Cam_Visualization(pl.Callback):
     def __init__(self, Datamodule,
@@ -44,16 +37,20 @@ class Cam_Visualization(pl.Callback):
         #train_loader = self.Datamodule.deterministic_train_dataloader()
         test_loader = self.Datamodule.test_dataloader() # use test loader as there is less data augmentation for the test loader
         
-        self.get_visualization(pl_module, test_loader)
+        self.get_visualization(trainer,pl_module, test_loader)
 
-    def get_visualization(self, pl_module, dataloader):
+    def get_visualization(self,trainer, pl_module, dataloader):
         loader = quickloading(self.quick_callback, dataloader)
+        
+
+        
         for index, (img, *label, indices) in enumerate(loader):
             assert len(loader)>0, 'loader is empty'
             if isinstance(img, tuple) or isinstance(img, list):
                     img, *aug_img = img # Used to take into accoutn whether the data is a tuple of the different augmentations
             
             img = img.to(pl_module.device)
+            #target_layers = [pl_module.encoder.layer4[-1]]
             target_layers = [pl_module.encoder.layer4[-1]]
 
             # If None, returns the map for the highest scoring category.
@@ -61,7 +58,7 @@ class Cam_Visualization(pl.Callback):
             target_category = None
 
             cam = GradCAM(pl_module.encoder,target_layers=target_layers,use_cuda= True)
-            cam.batch_size = 16
+            cam.batch_size = 32
 
             # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
             grayscale_cam = cam(input_tensor=img)
@@ -74,8 +71,21 @@ class Cam_Visualization(pl.Callback):
             mean, std = self.Datamodule.test_transforms.normalization.mean, self.Datamodule.test_transforms.normalization.std 
             
             visualization = show_cam_on_image(rgb_img, grayscale_cam,mean, std, use_rgb=True)            
-
-            '''            
-            img = Image.fromarray(visualization[0], 'RGB')
-            img.show()
-            '''
+            # https://docs.wandb.ai/guides/track/log/media
+            images = [Image.fromarray(image) for image in visualization]
+          
+            wandb.log({"GradCam Heatmaps": [wandb.Image(image) for image in images]})
+            
+            # visualization = torch.tensor(visualization) # shape (batch, H,W,C)
+            # 
+            # visualization = visualization.reshape(visualization.shape[0],visualization.shape[3], visualization.shape[1], visualization.shape[2]) # shape (batch,C, H,W)
+            # visualization = visualization.to(dtype=torch.float32)
+            # grid_imgs = torchvision.utils.make_grid(visualization)    
+            # images = wandb.Image(grid_imgs, caption="Top: Input, Bottom: Counterfactual")
+            # wandb.log({self.logging: images})
+            
+            # '''
+            # https://stackoverflow.com/questions/2659312/how-do-i-convert-a-numpy-array-to-and-display-an-image            
+            # img = Image.fromarray(visualization[0], 'RGB')
+            # img.show()
+            # '''
