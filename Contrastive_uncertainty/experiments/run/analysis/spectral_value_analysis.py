b90ed1b957dec76ae2bec6c08b0aac73c97b363b
nerdk312 <nawid.keshtmand@hotmail.com>
2022-04-14 10:31:24 +0100

    uupdated the code for the analysis, also commiting before updating the data tables for the coross-entropy comparison

--- Contrastive_uncertainty/experiments/run/analysis/spectral_value_analysis.py
+++ Contrastive_uncertainty/experiments/run/analysis/spectral_value_analysis.py
@@ -27,7 +27,8 @@ def obtain_spectral_values():
     parameter1 = 'rho_spectrum@1: instance: fine'
     parameter2 = 'rho_spectrum@2: instance: fine'
     #data_array = np.empty((5,3))
-    data_array = np.empty((6,3))
+    #data_array = np.empty((6,3))
+    data_array = np.empty((7,3))
     data_array[:] = np.nan
     for dataset in key_dict['dataset'].keys():
     
@@ -54,29 +55,34 @@ def obtain_spectral_values():
     
 
     #runs = api.runs(path="nerdk312/evaluation", filters={"config.group":"OOD hierarchy baselines",'config.seed':42,'config.epochs':300,'config.dataset':dataset})
-    
-    runs = api.runs(path="nerdk312/evaluation", filters={"config.group":"Baselines Repeats", 'config.seed':42,"config.dataset": "TinyImageNet","config.epochs":300})
+    additional_key_dict = {'model_type':{'CE':0, 'Moco':1, 'SupCon':2},
+                'dataset': {'Caltech256':5,'TinyImageNet':6}}    
     parameter1 = 'rho_spectrum@1'
-    summary_list, config_list = [], []
-    for i, run in enumerate(runs):
-        summary_list.append(run.summary._json_dict)    
-        # .config contains the hyperparameters.
-        #  We remove special values that start with _.
-        config_list.append(
-            {k: v for k,v in run.config.items()
-             if not k.startswith('_')})
+    for dataset in additional_key_dict['dataset'].keys():
+        runs = api.runs(path="nerdk312/evaluation", filters={"config.group":"Baselines Repeats", 'config.seed':42,"config.dataset": dataset,"config.epochs":300})
 
-        ID_dataset = config_list[i]['dataset']
-        model_type = config_list[i]['model_type']
-        value = abs(summary_list[i][parameter1])  #- summary_list[i][parameter2]) 
-        column = key_dict['model_type'][model_type]
-        row = 5
-        data_array[row, column] = np.around(value,decimals=3)
+        summary_list, config_list = [], []
+        for i, run in enumerate(runs):
+            summary_list.append(run.summary._json_dict)    
+            # .config contains the hyperparameters.
+            #  We remove special values that start with _.
+            config_list.append(
+                {k: v for k,v in run.config.items()
+                 if not k.startswith('_')})
+
+            ID_dataset = config_list[i]['dataset']
+            model_type = config_list[i]['model_type']
+            value = abs(summary_list[i][parameter1])  #- summary_list[i][parameter2]) 
+            column = additional_key_dict['model_type'][model_type]
+            row = additional_key_dict['dataset'][ID_dataset]
+            data_array[row, column] = np.around(value,decimals=3)
     # Obtain the names of the rows and the name of the columns
     column_names = ['SupCLR' if model =='SupCon' else model for model in key_dict['model_type'].keys()]
     row_names = [dataset for dataset in key_dict['dataset'].keys()]
+    additional_row_names = [dataset for dataset in additional_key_dict['dataset'].keys()]
 
-    row_names.extend(['TinyImageNet'])
+    #row_names.extend(['TinyImageNet'])
+    row_names.extend(additional_row_names)
     data_df = pd.DataFrame(data_array, columns = column_names, index = row_names)
     ax =data_df.plot.bar()
     plt.title('Spectral Decay for different datasets')
